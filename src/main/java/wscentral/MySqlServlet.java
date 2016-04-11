d62d9979c1e36b379eb6ca1541aba2af1382222f
Alberto <amarques74@gmail.com>
2016-04-11 21:49:39 +0200

    pp

--- src/main/java/wscentral/MySqlServlet.java
+++ src/main/java/wscentral/MySqlServlet.java
@@ -6,13 +6,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
-public class MySqlServlet {
+public class MySqlServlet  extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest request,
 	        HttpServletResponse response) throws ServletException, IOException {
